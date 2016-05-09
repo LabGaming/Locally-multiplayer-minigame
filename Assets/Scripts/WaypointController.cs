0cf6037435f457c41da3a1199c8bdac850ebd682
jordan.holtz <jordan.holtz@globant.com>
2016-05-09 19:33:40 -0300

    waypoints added

---
+++ Assets/Scripts/WaypointController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Com.LuisPedroFonseca.ProCamera2D;
+
+public class WaypointController : MonoBehaviour {
+
+	public GameObject[] players;
+	public GameObject[] waypoints;
+	public ProCamera2D procamera;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < players.Length ; i++) {
+			Transform transform = PlacePlayerInWaypoint (i, i);
+			procamera.AddCameraTarget (transform);
+		}	
+	}
+	
+	Transform PlacePlayerInWaypoint(int playerNumber, int waypointNumber){
+		Object go = Instantiate (players [playerNumber], 
+			waypoints [waypointNumber].transform.position, 
+			waypoints [waypointNumber].transform.rotation);
+		return (go as GameObject).transform;
+	}
+}
